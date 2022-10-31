19f70a7de866fb611988e4892b021012c67532c9
am456 <angem456@gmail.com>
2022-10-31 17:46:31 -0400

    Modified the client.py file's getDataPoint() method to return the correct value for its variable price. Updated the main method within this class to create a library of prices and to print the correct ratios.

--- client.py
+++ client.py
@@ -34,6 +34,7 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
+    # Variable price now represents a value found through the correct formula
     price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
@@ -51,7 +52,7 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # Variable prices holds a dictionary of values
         prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
